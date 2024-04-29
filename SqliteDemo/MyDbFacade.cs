1841970c27635bee3bc9b1a72260c0b0f06a7a98
Neznakomec <misha-ershov@ya.ru>
2024-04-29 14:49:02 +0300

    add sqlite

--- SqliteDemo/MyDbFacade.cs
+++ SqliteDemo/MyDbFacade.cs
@@ -1,10 +1,35 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SqliteDemo.Persistence.Entities;
+using System.Data;
+using System.Data.Common;
 
 namespace SqliteDemo
 {
     public sealed class DatabaseContext : DbContext
     {
+        private readonly DbConnection _connection;
+
         public DbSet<PersistedFill> Fills { get; set; }
+
+        public DatabaseContext(DbConnection connection, bool ownsConnection = true)
+        {
+            _connection = connection;
+        }
+
+        public DatabaseContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (_connection != null && _connection.State == ConnectionState.Open)
+            {
+                options.UseSqlite(_connection);
+            }
+            else
+            {
+                options.UseSqlite("Data Source=:memory:;");
+            }
+        }
     }
 }
---
+++ SqliteDemo/Persistence/PositionsRepository.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqliteDemo.Persistence
+{
+    public class PositionsRepository
+    {
+        private const string DbFileName = "positions.db";
+
+        private SqliteConnection _connection;
+
+        public bool IsSyncEnabled { get; set; }
+
+        public void InitializeAsync()
+        {
+            try
+            {
+                string appFolder = AppDomain.CurrentDomain.BaseDirectory;
+                string dbFolder = Path.Combine(appFolder, "db");
+                string dbFilePath = Path.Combine(dbFolder, "positions.db");
+
+                string connectionString = $"Data Source = \"{dbFilePath}\";";
+                EnsureDirectoryExists(dbFolder);
+                _connection = new SqliteConnection(connectionString);
+                _connection.Open();
+                CreateContext().Database.Migrate();
+                IsSyncEnabled = true;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("InitializeAsync" + exception + "Failed while creating PositionsRepository");
+            }
+
+        }
+
+        private void EnsureDirectoryExists(string dbDirectoryPath)
+        {
+            if (!Directory.Exists(dbDirectoryPath))
+            {
+                try
+                {
+                    Console.WriteLine("EnsureDirectoryExists: " + $"Directory {dbDirectoryPath} doesn't exist, trying to create");
+                    Directory.CreateDirectory(dbDirectoryPath);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine("EnsureDirectoryExists" + exception + $"Failed to create directory {dbDirectoryPath}");
+                    throw;
+                }
+            }
+        }
+
+        private DatabaseContext CreateContext()
+        {
+            return new DatabaseContext(_connection);
+        }
+    }
+}
